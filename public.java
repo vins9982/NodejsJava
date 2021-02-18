a996c336ec76483f75962114dc937ebee5c8a12e
vins9982 <17459350+vins9982@users.noreply.github.com>
2021-02-18 13:15:41 -0500

    sample12

--- public.java
+++ public.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String args[]) {
     String firstName = "John2";
-    String lastName = "Doe";
+    String lastName = "Doe1";
     System.out.println(firstName + " " + lastName);
   }
 }
--- samplespund.js
+++ samplespund.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var rs = fs.createReadStream('./demofile1.txt');
 rs.on('open', function () {
-  console.log('The file is opened');
+  console.log('The file is opened1');
 });
